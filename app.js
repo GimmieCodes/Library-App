31cd320c3fda6c9eebe786ace9c377eff84d4a23
Dan <steezym279@gmail.com>
2024-01-17 11:26:46 -0600

    Create logic to loop through liibrary array and print out on webpage

--- app.js
+++ app.js
@@ -13,18 +13,23 @@ let lib = []
 const main = document.querySelector(".main")
 
 function addBookToLibrary(title,author,pages,read){
-    const card = document.createElement("div")
-    card.classList.add("card")
+
     lib.push(new Book(title,author,pages,read))
-    main.appendChild(card)
-    const book = new Book(title,author,pages,read)
-    const titleCard = document.createElement("h2")
-    titleCard.textContent = `${book.title}`;
-    card.appendChild(titleCard)
+
 
 }
+
 addBookToLibrary("The Hobbit","J.R.R. Tolkien","295 pages","not read")
 
+function displayBookToLibrary(){
+    const titleCard = document.createElement("h2")
+    for(let i = 0; i < lib.length; i++){
+        titleCard.textContent = `${lib[i].title} by ${lib[i].author}, ${lib[i].pages}, ${lib[i].read}`
+        main.appendChild(titleCard)
+
+    }
+}
+displayBookToLibrary()
 const addBookPromptButton = document.querySelector(".addBookPromptButton")
 const closePrompt = document.getElementById("closePrompt")
 const body = document.querySelector("body")
@@ -36,3 +41,10 @@ addBookPromptButton.addEventListener('click',function(){
 closePrompt.addEventListener('click',function(){
     document.getElementById('bookPrompt').style.display='none';
 })
+
+
+//main.appendChild(card)
+//const book = new Book(title,author,pages,read)
+//const titleCard = document.createElement("h2")
+//titleCard.textContent = `${book.title}`;
+//card.appendChild(titleCard)
