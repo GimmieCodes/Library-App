e2954f7661584a33dea4882dd53ede0d2791ca33
Dan <steezym279@gmail.com>
2024-01-17 19:36:23 -0600

    Add functionality to submit to create cards for books and close prompt after submission

--- app.js
+++ app.js
@@ -4,58 +4,72 @@ function Book(title,author,pages,read){
     this.pages = pages
     this.read = read
     this.info = function(){
-        return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+        return `${this.title} by ${this.author}, ${this.pages} , ${this.read}`
     }
 }
 
-let lib = [{title:"awesome book",author:"awesome writer dude",pages:"21",read:"read"}]
+let lib = []
 
 const main = document.querySelector(".main")
 
 function addBookToLibrary(title,author,pages,read){
-
     lib.push(new Book(title,author,pages,read))
-
-
+    displayBookToLibrary()
 }
 
-addBookToLibrary("The Hobbit","J.R.R. Tolkien","295 pages","not read")
+
 
 function displayBookToLibrary(){
+    main.innerHTML = '';
     for(let i = 0; i < lib.length; i++){
         const titleCard = document.createElement("h2")
         const authorCard = document.createElement("h2")
         const pagesCard = document.createElement("h2")
-        const readCard = document.createElement("h2")
+        const readCard = document.createElement("button")
         const card = document.createElement("div")
-        const cardReadBtn = document.createElement("button")
         const cardRemoveBtn = document.createElement("button")
-        titleCard.textContent = `${lib[i].title}`
+        const readBtn = document.querySelector(".readBtn")
+        titleCard.textContent = `"${lib[i].title}"`
         authorCard.textContent = `by ${lib[i].author}`
-        pagesCard.textContent = `${lib[i].pages}`
-        readCard.textContent = `by ${lib[i].read}`
-        cardReadBtn.textContent = "Read"
+        pagesCard.textContent = `${lib[i].pages} Pages`
+        readCard.textContent = `${lib[i].read}`
         cardRemoveBtn.textContent = "remove"
         main.appendChild(card)
         card.classList.add(`card`)
+        readCard.classList.add(`readBtn`)
         card.appendChild(titleCard)
         card.appendChild(authorCard)
         card.appendChild(pagesCard)
         card.appendChild(readCard)
-        card.appendChild(cardReadBtn)
         card.appendChild(cardRemoveBtn)
+        
 
     }
 }
-displayBookToLibrary()
 const addBookPromptButton = document.querySelector(".addBookPromptButton")
 const closePrompt = document.getElementById("closePrompt")
 const body = document.querySelector(".body")
 
 document.querySelector(".submitBtn").addEventListener("click",function(event){
     event.preventDefault()
+    const title = document.getElementById("bookName").value
+    const author = document.getElementById("bookAuthor").value
+    const pages = document.getElementById("bookPages").value
+    readCheck()
+    addBookToLibrary(title,author,pages,read)
+    document.getElementById('bookPrompt').style.display='none';
 })
-
+let read = document.getElementById("bookReadStatus")
+function readCheck(){
+    const pages = document.getElementById("bookPages").value
+    
+    if (read.checked == true){
+         return read = "Has Read"
+         
+    }else{
+       return read = "Has not Read"
+    }
+}
 addBookPromptButton.addEventListener('click',function(){
     document.getElementById('bookPrompt').style.display='block';
 
@@ -63,6 +77,10 @@ addBookPromptButton.addEventListener('click',function(){
 closePrompt.addEventListener('click',function(){
     document.getElementById('bookPrompt').style.display='none';
 })
+
+displayBookToLibrary()
+
+
 console.log(lib)
 
 //main.appendChild(card)
@@ -70,3 +88,9 @@ console.log(lib)
 //const titleCard = document.createElement("h2")
 //titleCard.textContent = `${book.title}`;
 //card.appendChild(titleCard)
+
+//main.addEventListener('click',function(){
+   // if(event.target.classList.contains('readBtn')){
+    //    return read = "Has not Read"
+  //  }
+//})
