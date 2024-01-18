6ec314abd22c7a3558530383be3c1e12756e7c3f
Dan <steezym279@gmail.com>
2024-01-18 16:28:30 -0600

    Create logic to toggle read and not read status as well as a functional remove button

--- app.js
+++ app.js
@@ -28,27 +28,43 @@ function displayBookToLibrary(){
         const readCard = document.createElement("button")
         const card = document.createElement("div")
         const cardRemoveBtn = document.createElement("button")
-        const readBtn = document.querySelector(".readBtn")
         titleCard.textContent = `"${lib[i].title}"`
         authorCard.textContent = `by ${lib[i].author}`
         pagesCard.textContent = `${lib[i].pages} Pages`
         readCard.textContent = `${lib[i].read}`
         cardRemoveBtn.textContent = "remove"
+        card.dataset.card = [i]
         main.appendChild(card)
         card.classList.add(`card`)
         readCard.classList.add(`readBtn`)
+        cardRemoveBtn.classList.add(`cardRemoveBtn`)
         card.appendChild(titleCard)
         card.appendChild(authorCard)
         card.appendChild(pagesCard)
         card.appendChild(readCard)
         card.appendChild(cardRemoveBtn)
-        
+        readCard.addEventListener('click',function(){
+            if (readCard.textContent == "Read"){
+                return readCard.textContent = "Not Read"
+            }else if(readCard.textContent == "Not Read"){
+                    return readCard.textContent = "Read"
+                
+            }
+        })
+        cardRemoveBtn.addEventListener('click',function(){
+            const removeIndex = card.dataset.card
+            lib.splice(removeIndex,1)
+            displayBookToLibrary();
+       })
 
     }
 }
 const addBookPromptButton = document.querySelector(".addBookPromptButton")
 const closePrompt = document.getElementById("closePrompt")
 const body = document.querySelector(".body")
+const cardRemoveBtn = document.querySelector(".cardRemoveBtn")
+
+
 
 document.querySelector(".submitBtn").addEventListener("click",function(event){
     event.preventDefault()
@@ -59,17 +75,20 @@ document.querySelector(".submitBtn").addEventListener("click",function(event){
     addBookToLibrary(title,author,pages,read)
     document.getElementById('bookPrompt').style.display='none';
 })
+
 let read = document.getElementById("bookReadStatus")
+
 function readCheck(){
-    const pages = document.getElementById("bookPages").value
+    
     
     if (read.checked == true){
-         return read = "Has Read"
+         return read = "Read"
          
     }else{
-       return read = "Has not Read"
+       return read = "Not Read"
     }
 }
+
 addBookPromptButton.addEventListener('click',function(){
     document.getElementById('bookPrompt').style.display='block';
 
